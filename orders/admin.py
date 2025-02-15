bdabb6729b6f9503041bc8737d36b2d7dea3dc1b
examplesOfWorks <webDissss@gmail.com>
2025-02-16 01:24:53 +0300

    создание моделей

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Meals, Orders, OrderItems
 
-# Register your models here.
+@admin.register(Meals)
+class MealsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('table_number', 'total_price', 'status', 'created_timestamp')
+
+
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ('order', 'meal', 'quantity')
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Meals(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название блюда')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена') 
+
+    class Meta:
+        db_table = 'meal'
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"Название: {self.name} | Цена: {self.price}"
+   
+        
+class Orders(models.Model):
+    table_number = models.IntegerField(verbose_name='Номер стола')
+    total_price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Общая цена')
+    status = models.CharField(max_length=100, verbose_name='Статус')
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        db_table = 'order'
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"Заказ № {self.pk} | Стол: {self.table_number} | Сумма: {self.total_price} | Статус: {self.status} | Дата: {self.created_timestamp}"
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(to=Orders, on_delete=models.CASCADE, verbose_name='Заказ')
+    meal = models.ForeignKey(to=Meals, on_delete=models.CASCADE, verbose_name='Блюдо')
+    quantity = models.IntegerField(verbose_name='Количество')
+    item_price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена позиции')
+
+    class Meta:
+        db_table = 'order_item'
+        verbose_name = 'Позиция заказа'
+        verbose_name_plural = 'Позиции заказа'
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"Заказ № {self.order} | Блюдо: {self.meal} | Количество: {self.quantity} | Цена позиции: {self.item_price}"
